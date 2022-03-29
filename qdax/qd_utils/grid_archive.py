ab7a739299670683fe910cc6f1229d6881e82908
Maxime Allard <m.allard20@imperial.ac.uk>
2022-03-29 20:04:57 +0200

    Adding First Occurring Solutions Correctly
    Fixing Issue when adding individuals with the same BD for the first time to check for the best solution amongst these.

--- qdax/qd_utils/grid_archive.py
+++ qdax/qd_utils/grid_archive.py
@@ -48,10 +48,11 @@ class Repertoire:
         bd_indexes = jnp.ravel_multi_index(bd_cells, repertoire.bd.shape,mode = 'clip')
         maximum_fitness = jax.ops.segment_max(eval_scores, bd_indexes, num_segments=repertoire.fitness.ravel().shape[0])
         eval_scores_filtered = jnp.where(maximum_fitness.at[bd_indexes].get()==eval_scores,eval_scores,np.iinfo(np.int32).min)
+        keep_eval_scores_filtered = jnp.where(maximum_fitness.at[bd_indexes].get()==eval_scores,True,False)
         # Checking Conditions for fitness function
         current_fitness = repertoire.fitness.ravel().at[bd_indexes].get()
         # Checking if fitness function is nan or not, since nan means we do not have an individual yet
-        current_fitness_nan = jnp.isnan(current_fitness)
+        current_fitness_nan = jnp.logical_and(jnp.isnan(current_fitness),keep_eval_scores_filtered)
         # Checking if fitness that we have is better than the one we observed
         better_fitness = current_fitness < eval_scores_filtered
 
@@ -65,9 +66,9 @@ class Repertoire:
 
         #Every Individual that is not valid will be assigned index 100000 because we cannot cut our arrays. Jit needs to know the size of the array.
         #When adding, every individual will be clipped and sent to the same location
-        mult_to_be_added = jnp.where(to_be_added,1,100000)
+        mult_to_be_added = jnp.where(to_be_added,0,100000)
 
-        bd_insertion = bd_indexes * mult_to_be_added
+        bd_insertion = bd_indexes + mult_to_be_added
 
         # Adding individuals indivs to grid
         leaves = []
