3358d1cb6859104c8825b8ae1b192bf24c4c7796
limbryan <limbryan239@gmail.com>
2022-06-27 19:12:36 +0100

    Extend the functionality of compute_euclidean_centroids to be more general to any dimension grid and any number of centroids per dimension

--- qdax/core/containers/repertoire.py
+++ qdax/core/containers/repertoire.py
@@ -58,7 +58,7 @@ def compute_cvt_centroids(
 
 def compute_euclidean_centroids(
     num_descriptors: int,
-    num_centroids: int,
+    grid_shape: List[int],
     minval: Union[float, List[float]],
     maxval: Union[float, List[float]],
 ) -> jnp.ndarray:
@@ -66,30 +66,29 @@ def compute_euclidean_centroids(
     Compute centroids for square Euclidean tesselation.
 
     Args:
-        num_descriptors: number od scalar descriptors
-        num_centroids: number of centroids
+        num_descriptors: number of scalar descriptors
+        grid_shape: number of centroids per BD dimension
         minval: minimum descriptors value
         maxval: maximum descriptors value
 
     Returns:
         the centroids with shape (num_centroids, num_descriptors)
     """
-    if num_descriptors != 2:
-        raise NotImplementedError("This function supports 2 descriptors only for now.")
 
-    sqrt_centroids = math.sqrt(num_centroids)
+    assert num_descriptors == len(grid_shape)
+    
+    linspace_list = []
+    for num_centroids_in_dim in grid_shape:
+        offset = 1 / (2 * int(num_centroids_in_dim))
+        linspace = jnp.linspace(offset, 1.0 - offset, int(num_centroids_in_dim))
+        linspace_list.append(linspace)
 
-    if math.floor(sqrt_centroids) != sqrt_centroids:
-        raise ValueError("Num centroids should be a squared number.")
-
-    offset = 1 / (2 * int(sqrt_centroids))
-
-    linspace = jnp.linspace(offset, 1.0 - offset, int(sqrt_centroids))
-    meshes = jnp.meshgrid(linspace, linspace, sparse=False)
-    centroids = jnp.stack([jnp.ravel(meshes[0]), jnp.ravel(meshes[1])], axis=-1)
+    meshes = jnp.meshgrid(*[linspace for linspace in linspace_list], sparse=False)
+    centroids = jnp.stack([jnp.ravel(meshes[i]) for i in range(num_descriptors)], axis=-1)
     minval = jnp.array(minval)
     maxval = jnp.array(maxval)
     return jnp.asarray(centroids) * (maxval - minval) + minval
+
 
 
 def get_cells_indices(
