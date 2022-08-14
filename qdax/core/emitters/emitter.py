7b214a1d900a866b5378f3bc8e61c4ca03bd9c9d
Luca Grillotti <luca.grillotti@gmail.com>
2022-08-14 17:54:58 +0100

    fix implementation MultiEmitter

--- qdax/core/emitters/emitter.py
+++ qdax/core/emitters/emitter.py
@@ -145,30 +145,30 @@ class MultiEmitter(Emitter):
 
     def state_update(
         self,
-        multi_emitter_state: Optional[MultiEmitterState],
+        emitter_state: Optional[MultiEmitterState],
         repertoire: Optional[Repertoire] = None,
         genotypes: Optional[Genotype] = None,
         fitnesses: Optional[Fitness] = None,
         descriptors: Optional[Descriptor] = None,
         extra_scores: Optional[ExtraScores] = None,
     ) -> Optional[MultiEmitterState]:
-        if multi_emitter_state is None:
+        if emitter_state is None:
             return None
 
         list_emitter_states = []
 
-        for emitter, emitter_state in zip(
-            self.emitters_tuple, multi_emitter_state.emitters_state_tuple
+        for emitter, sub_emitter_state in zip(
+            self.emitters_tuple, emitter_state.emitters_state_tuple
         ):
-            emitter_state = emitter.state_update(
-                multi_emitter_state,
+            sub_emitter_state = emitter.state_update(
+                emitter_state,
                 repertoire,
                 genotypes,
                 fitnesses,
                 descriptors,
                 extra_scores,
             )
-            list_emitter_states.append(emitter_state)
+            list_emitter_states.append(sub_emitter_state)
 
         return MultiEmitterState(tuple(list_emitter_states))
 
