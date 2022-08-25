8c9017eea88e266497533afe9f7ab5dfcf6961f9
Luca Grillotti <luca.grillotti16@imperial.ac.uk>
2022-08-25 09:40:46 +0000

    update packages dependencies

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ setup(
     long_description_content_type="text/markdown",
     install_requires=[
         "absl-py>=1.0.0",
-        "jax>=0.3.10",
-        "jaxlib>=0.3.10",  # necessary to build the doc atm
-        "flax>=0.4.1",
+        "jax>=0.3.16",
+        "jaxlib>=0.3.15",  # necessary to build the doc atm
+        "flax>=0.6.0",
         "brax>=0.0.12",
         "gym>=0.23.1",
         "numpy>=1.22.3",
