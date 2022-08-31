8a6d966e8c914fafdb2ea70b7c8cd110f776be39
Luca Grillotti <luca.grillotti@gmail.com>
2022-08-31 12:18:48 +0100

    add SBX cross-over and Polynomial Mutation

--- qdax/core/emitters/mutation_operators.py
+++ qdax/core/emitters/mutation_operators.py
@@ -1,246 +1,22 @@
 """File defining mutation and crossover functions."""
 import abc
 import math
-from functools import partial
 from typing import List, Optional, Tuple
 
 import jax
 import jax.numpy as jnp
 from chex import ArrayTree
+from matplotlib import pyplot as plt
 
 from qdax.core.containers.repertoire import Repertoire
 from qdax.core.emitters.emitter import Emitter, EmitterState
 from qdax.types import Genotype, RNGKey
 
 
-def _polynomial_mutation(
-    x: jnp.ndarray,
-    random_key: RNGKey,
-    proportion_to_mutate: float,
-    eta: float,
-    minval: float,
-    maxval: float,
-) -> jnp.ndarray:
-    """Base polynomial mutation for one genotype.
-
-    Proportion to mutate between 0 and 1
-    Assumed to be of shape (genotype_dim,)
-
-    Args:
-        x: parameters.
-        random_key: a random key
-        proportion_to_mutate: the proportion of the given parameters
-            that need to be mutated.
-        eta: the inverse of the power of the mutation applied.
-        minval: range of the perturbation applied by the mutation.
-        maxval: range of the perturbation applied by the mutation.
-
-    Returns:
-        New parameters.
-    """
-
-    # Select positions to mutate
-    num_positions = x.shape[0]
-    positions = jnp.arange(start=0, stop=num_positions)
-    num_positions_to_mutate = int(proportion_to_mutate * num_positions)
-    random_key, subkey = jax.random.split(random_key)
-    selected_positions = jax.random.choice(
-        key=subkey, a=positions, shape=(num_positions_to_mutate,), replace=False
-    )
-
-    # New values
-    mutable_x = x[selected_positions]
-    delta_1 = (mutable_x - minval) / (maxval - minval)
-    delta_2 = (maxval - mutable_x) / (maxval - minval)
-    mutpow = 1.0 / (1.0 + eta)
-
-    # Randomly select where to put delta_1 and delta_2
-    random_key, subkey = jax.random.split(random_key)
-    rand = jax.random.uniform(
-        key=subkey,
-        shape=delta_1.shape,
-        minval=0,
-        maxval=1,
-        dtype=jnp.float32,
-    )
-
-    value1 = 2.0 * rand + (jnp.power(delta_1, 1.0 + eta) * (1.0 - 2.0 * rand))
-    value2 = 2.0 * (1 - rand) + 2.0 * (jnp.power(delta_2, 1.0 + eta) * (rand - 0.5))
-    value1 = jnp.power(value1, mutpow) - 1.0
-    value2 = 1.0 - jnp.power(value2, mutpow)
-
-    delta_q = jnp.zeros_like(mutable_x)
-    delta_q = jnp.where(rand < 0.5, value1, delta_q)
-    delta_q = jnp.where(rand >= 0.5, value2, delta_q)
-
-    # Mutate values
-    x = x.at[selected_positions].set(mutable_x + (delta_q * (maxval - minval)))
-
-    # Back in bounds if necessary (floating point issues)
-    x = jnp.clip(x, minval, maxval)
-
-    return x
-
-
-def polynomial_mutation(
-    x: Genotype,
-    random_key: RNGKey,
-    proportion_to_mutate: float,
-    eta: float,
-    minval: float,
-    maxval: float,
-) -> Tuple[Genotype, RNGKey]:
-    """
-    Polynomial mutation over several genotypes
-
-    Parameters:
-        x: array of genotypes to transform (real values only)
-        random_key: RNG key for reproducibility.
-            Assumed to be of shape (batch_size, genotype_dim)
-        proportion_to_mutate (float): proportion of variables to mutate in
-            each genotype (must be in [0, 1]).
-        eta: scaling parameter, the larger the more spread the new
-            values will be.
-        minval: minimum value to clip the genotypes.
-        maxval: maximum value to clip the genotypes.
-
-    Returns:
-        New genotypes - same shape as input and a new RNG key
-    """
-    random_key, subkey = jax.random.split(random_key)
-    batch_size = jax.tree_leaves(x)[0].shape[0]
-    mutation_key = jax.random.split(subkey, num=batch_size)
-    mutation_fn = partial(
-        _polynomial_mutation,
-        proportion_to_mutate=proportion_to_mutate,
-        eta=eta,
-        minval=minval,
-        maxval=maxval,
-    )
-    mutation_fn = jax.vmap(mutation_fn)
-    x = jax.tree_map(lambda x_: mutation_fn(x_, mutation_key), x)
-    return x, random_key
-
-
-def _polynomial_crossover(
-    x1: jnp.ndarray,
-    x2: jnp.ndarray,
-    random_key: RNGKey,
-    proportion_var_to_change: float,
-) -> jnp.ndarray:
-    """
-    Base crossover for one pair of genotypes.
-
-    x1 and x2 should have the same shape
-    In this function we assume x1 shape and x2 shape to be (genotype_dim,)
-    """
-    num_var_to_change = int(proportion_var_to_change * x1.shape[0])
-    indices = jnp.arange(start=0, stop=x1.shape[0])
-    selected_indices = jax.random.choice(
-        random_key, indices, shape=(num_var_to_change,)
-    )
-    x = x1.at[selected_indices].set(x2[selected_indices])
-    return x
-
-
-def polynomial_crossover(
-    x1: Genotype,
-    x2: Genotype,
-    random_key: RNGKey,
-    proportion_var_to_change: float,
-) -> Tuple[Genotype, RNGKey]:
-    """
-    Crossover over a set of pairs of genotypes.
-
-    Batched version of _simple_crossover_function
-    x1 and x2 should have the same shape
-    In this function we assume x1 shape and x2 shape to be
-    (batch_size, genotype_dim)
-
-    Parameters:
-        x1: first batch of genotypes
-        x2: second batch of genotypes
-        random_key: RNG key for reproducibility
-        proportion_var_to_change: proportion of variables to exchange
-            between genotypes (must be [0, 1])
-
-    Returns:
-        New genotypes and a new RNG key
-    """
-
-    random_key, subkey = jax.random.split(random_key)
-    batch_size = jax.tree_leaves(x2)[0].shape[0]
-    crossover_keys = jax.random.split(subkey, num=batch_size)
-    crossover_fn = partial(
-        _polynomial_crossover,
-        proportion_var_to_change=proportion_var_to_change,
-    )
-    crossover_fn = jax.vmap(crossover_fn)
-    # TODO: check that key usage is correct
-    x = jax.tree_map(lambda x1_, x2_: crossover_fn(x1_, x2_, crossover_keys), x1, x2)
-    return x, random_key
-
-
-def isoline_variation(
-    x1: Genotype,
-    x2: Genotype,
-    random_key: RNGKey,
-    iso_sigma: float,
-    line_sigma: float,
-    minval: Optional[float] = None,
-    maxval: Optional[float] = None,
-) -> Tuple[Genotype, RNGKey]:
-    """
-    Iso+Line-DD Variation Operator [1] over a set of pairs of genotypes
-
-    Parameters:
-        x1 (Genotypes): first batch of genotypes
-        x2 (Genotypes): second batch of genotypes
-        random_key (RNGKey): RNG key for reproducibility
-        iso_sigma (float): spread parameter (noise)
-        line_sigma (float): line parameter (direction of the new genotype)
-        minval (float, Optional): minimum value to clip the genotypes
-        maxval (float, Optional): maximum value to clip the genotypes
-
-    Returns:
-        x (Genotypes): new genotypes
-        random_key (RNGKey): new RNG key
-
-    [1] Vassiliades, Vassilis, and Jean-Baptiste Mouret. "Discovering the elite
-    hypervolume by leveraging interspecies correlation." Proceedings of the Genetic and
-    Evolutionary Computation Conference. 2018.
-    """
-
-    # Computing line_noise
-    random_key, key_line_noise = jax.random.split(random_key)
-    batch_size = jax.tree_leaves(x1)[0].shape[0]
-    line_noise = jax.random.normal(key_line_noise, shape=(batch_size,)) * line_sigma
-
-    def _variation_fn(
-        x1: jnp.ndarray, x2: jnp.ndarray, random_key: RNGKey
-    ) -> jnp.ndarray:
-        iso_noise = jax.random.normal(random_key, shape=x1.shape) * iso_sigma
-        x = (x1 + iso_noise) + jax.vmap(jnp.multiply)((x2 - x1), line_noise)
-
-        # Back in bounds if necessary (floating point issues)
-        if (minval is not None) or (maxval is not None):
-            x = jnp.clip(x, minval, maxval)
-        return x
-
-    # create a tree with random keys
-    nb_leaves = len(jax.tree_leaves(x1))
-    random_key, subkey = jax.random.split(random_key)
-    subkeys = jax.random.split(subkey, num=nb_leaves)
-    keys_tree = jax.tree_unflatten(jax.tree_structure(x1), subkeys)
-
-    # apply isolinedd to each branch of the tree
-    x = jax.tree_map(lambda y1, y2, key: _variation_fn(y1, y2, key), x1, x2, keys_tree)
-
-    return x, random_key
-
-
 class VariationOperator(metaclass=abc.ABCMeta):
     def __init__(self, minval: Optional[float] = None, maxval: Optional[float] = None):
+        if minval is not None and maxval is not None:
+            assert minval < maxval, "minval must be smaller than maxval"
         self._minval = minval
         self._maxval = maxval
 
@@ -311,6 +87,21 @@ class VariationOperator(metaclass=abc.ABCMeta):
         return keys_tree, random_key
 
     @staticmethod
+    def _get_array_keys_for_each_gen(key: RNGKey, gen_tree: Genotype) -> jnp.ndarray:
+        subkeys = jax.random.split(key, num=gen_tree.shape[0])
+        return jnp.asarray(subkeys)
+
+    @staticmethod
+    def get_keys_arrays_tree(
+        gen_tree: Genotype, random_key: RNGKey
+    ) -> Tuple[ArrayTree, RNGKey]:
+        keys_tree, random_key = VariationOperator.get_tree_keys(gen_tree, random_key)
+        keys_arrays_tree = jax.tree_map(
+            VariationOperator._get_array_keys_for_each_gen, keys_tree, gen_tree
+        )
+        return keys_arrays_tree, random_key
+
+    @staticmethod
     def _get_random_positions_to_change(
         genotypes_tree: Genotype,
         variation_rate: float,
@@ -328,23 +119,17 @@ class VariationOperator(metaclass=abc.ABCMeta):
             )
             return selected_positions
 
-        def _get_array_keys(_key: RNGKey, _gen: Genotype) -> jnp.ndarray:
-            _key, *_subkeys = jax.random.split(_key, num=_gen.shape[0] + 1)
-            return jnp.asarray(_subkeys)
-
         random_key, _subkey = jax.random.split(random_key)
-        print()
-        key_arrays_tree = jax.tree_map(
-            _get_array_keys,
-            VariationOperator.get_tree_keys(genotypes_tree, _subkey)[0],
-            genotypes_tree,
+
+        keys_arrays_tree, random_key = VariationOperator.get_keys_arrays_tree(
+            genotypes_tree, random_key
         )
 
         return (
             jax.tree_map(
                 jax.vmap(_get_indexes_positions_cross_over),
                 genotypes_tree,
-                key_arrays_tree,
+                keys_arrays_tree,
             ),
             random_key,
         )
@@ -447,7 +232,7 @@ class Mutation(VariationOperator, abc.ABC):
         ...
 
 
-class Normal(Mutation):
+class NormalMutation(Mutation):
     def __init__(
         self,
         sigma: float,
@@ -465,6 +250,62 @@ class Normal(Mutation):
             return _gen + jax.random.normal(key=_key, shape=_gen.shape) * self.sigma
 
         return jax.tree_map(_variation_fn, gen, array_keys), random_key
+
+
+class PolynomialMutation(Mutation):
+    def __init__(
+        self,
+        eta: float,
+        minval: float,
+        maxval: float,
+        mutation_rate: float = 1.0,
+    ):
+        # for polynomial mutation, minval and maxval must be specified and finite
+        super().__init__(mutation_rate=mutation_rate, minval=minval, maxval=maxval)
+        self._eta = eta
+
+    def _mutate(self, gen: Genotype, random_key: RNGKey) -> Tuple[Genotype, RNGKey]:
+        def _mutate_single_subgen_array(
+            _subgen_array: jnp.ndarray, _random_key: RNGKey
+        ) -> jnp.ndarray:
+            assert self._minval is not None and self._maxval is not None
+
+            delta_1 = (_subgen_array - self._minval) / (self._maxval - self._minval)
+            delta_2 = (self._maxval - _subgen_array) / (self._maxval - self._minval)
+            mutpow = 1.0 / (1.0 + self._eta)
+
+            # Randomly select where to put delta_1 and delta_2
+            _random_key, subkey = jax.random.split(_random_key)
+            rand = jax.random.uniform(
+                key=subkey,
+                shape=delta_1.shape,
+                minval=0,
+                maxval=1,
+                dtype=jnp.float32,
+            )
+
+            value1 = 2.0 * rand + (
+                jnp.power(delta_1, 1.0 + self._eta) * (1.0 - 2.0 * rand)
+            )
+            value2 = 2.0 * (1 - rand) + 2.0 * (
+                jnp.power(delta_2, 1.0 + self._eta) * (rand - 0.5)
+            )
+            value1 = jnp.power(value1, mutpow) - 1.0
+            value2 = 1.0 - jnp.power(value2, mutpow)
+
+            delta_q = jnp.zeros_like(_subgen_array)
+            delta_q = jnp.where(rand < 0.5, value1, delta_q)
+            delta_q = jnp.where(rand >= 0.5, value2, delta_q)
+
+            # Mutate values
+            new_subgen_array = _subgen_array + delta_q * (self._maxval - self._minval)
+            return new_subgen_array
+
+        keys_arrays_tree, random_key = self.get_keys_arrays_tree(gen, random_key)
+
+        new_gen = jax.tree_map(_mutate_single_subgen_array, gen, keys_arrays_tree)
+
+        return new_gen, random_key
 
 
 class CrossOver(VariationOperator, abc.ABC):
@@ -504,6 +345,7 @@ class CrossOver(VariationOperator, abc.ABC):
         subgen_2 = self._get_sub_genotypes(gen_2, selected_positions=selected_indices)
 
         if self.returns_single_genotype:
+
             new_subgen, random_key = self._cross_over(subgen_1, subgen_2, random_key)
             new_gen = self._set_sub_genotypes(gen_1, selected_indices, new_subgen)
             return new_gen, random_key
@@ -528,6 +370,86 @@ class CrossOver(VariationOperator, abc.ABC):
         self, gen_1: Genotype, gen_2: Genotype, random_key: RNGKey
     ) -> Tuple[Genotype, RNGKey]:
         ...
+
+
+class RecombinationCrossOver(CrossOver):
+    def _cross_over(
+        self, gen_original: Genotype, gen_exchange: Genotype, random_key: RNGKey
+    ) -> Tuple[Genotype, RNGKey]:
+        # The exchange cross over is a simple exchange of the two genotypes
+        # the proportion of the two genotypes that are changed is the
+        # same as the cross-over rate the parts which are exchanged are
+        # randomly selected in CrossOver
+        return gen_exchange, random_key
+
+
+class SBXCrossOver(CrossOver):
+    def __init__(
+        self,
+        eta: float,
+        minval: float,
+        maxval: float,
+        cross_over_rate: float = 1.0,
+        returns_single_genotype: bool = True,
+    ):
+        super().__init__(cross_over_rate, returns_single_genotype, minval, maxval)
+        self._eta = eta
+
+    def _cross_over(
+        self, gen_1: Genotype, gen_2: Genotype, random_key: RNGKey
+    ) -> Tuple[Genotype, RNGKey]:
+        def _crossover_single_subgen_array(
+            _subgen_array_1: jnp.ndarray,
+            _subgen_array_2: jnp.ndarray,
+            _random_key: RNGKey,
+        ) -> jnp.ndarray:
+            assert self._minval is not None and self._maxval is not None
+
+            normalized_gen_1 = (_subgen_array_1 - self._minval) / (
+                self._maxval - self._minval
+            )
+            normalized_gen_2 = (_subgen_array_2 - self._minval) / (
+                self._maxval - self._minval
+            )
+
+            y1 = jnp.minimum(normalized_gen_1, normalized_gen_2)
+            y2 = jnp.maximum(normalized_gen_1, normalized_gen_2)
+
+            yl = 0.0
+            yu = 1.0
+
+            _random_key, _subkey = jax.random.split(_random_key)
+            rand = jax.random.uniform(
+                key=_random_key, shape=y1.shape, minval=0, maxval=1, dtype=jnp.float32
+            )
+
+            beta = 1.0 + (2.0 * (y1 - yl) / (y2 - y1))
+            alpha = 2.0 - beta ** -(self._eta + 1.0)
+
+            alpha_rand = rand * alpha
+
+            betaq = jnp.where(
+                rand <= 1.0 / alpha,
+                (alpha_rand ** (1.0 / (self._eta + 1.0))),
+                (1.0 / (2.0 - alpha_rand)) ** (1.0 / (self._eta + 1.0)),
+            )
+
+            c1 = 0.5 * (y1 + y2) - 0.5 * (y2 - y1) * betaq
+
+            c1 = jnp.clip(c1, yl, yu)
+
+            c1 = c1 * (self._maxval - self._minval) + self._minval
+
+            return c1
+
+        keys_arrays_tree, random_key = self.get_keys_arrays_tree(gen_1, random_key)
+        new_gen = jax.tree_util.tree_map(
+            jax.vmap(_crossover_single_subgen_array),
+            gen_1,
+            gen_2,
+            keys_arrays_tree,
+        )
+        return new_gen, random_key
 
 
 class IsolineVariationOperator(CrossOver):
@@ -646,11 +568,10 @@ class SelectionVariationEmitter(Emitter):
 
 
 def _test() -> None:
-    v1 = Normal(
-        mutation_rate=0.5,
-        sigma=0.1,
-        minval=None,
-        maxval=None,
+    v1 = PolynomialMutation(
+        eta=20,
+        minval=0.0,
+        maxval=1.0,
     )
     v2 = IsolineVariationOperator(
         iso_sigma=0.1,
@@ -660,11 +581,28 @@ def _test() -> None:
         minval=None,
         maxval=None,
     )
-    v = ComposerVariations(variations_operators_list=[v1, v2], minval=None, maxval=None)
-    gen_1 = {"a": jnp.zeros((2, 10))}
+    v4 = SBXCrossOver(
+        eta=20.0,
+        minval=0.0,
+        maxval=1.0,
+    )
+    v3 = RecombinationCrossOver(cross_over_rate=0.5, returns_single_genotype=False)
+    v = ComposerVariations(
+        variations_operators_list=[v1, v2, v3, v4], minval=None, maxval=None
+    )
+    gen_1 = {
+        "a": jnp.concatenate(
+            [0.3 * jnp.ones((1, 300)), 0.7 * jnp.ones((1, 300))], axis=0
+        )
+    }
     # gen_2 = {"a": jnp.array([[0., -1.], [0.5, 0.5]])}
-    random_key = jax.random.PRNGKey(4)
-    print(v.apply_with_clip(gen_1, None, random_key)[0])
+    random_key = jax.random.PRNGKey(5)
+    res = v.apply_with_clip(gen_1, None, random_key)[0]["a"]
+
+    fig, ax = plt.subplots()
+    ax.scatter(res[0, :], jnp.zeros_like(res[0, :]), s=1, linewidths=0)
+    ax.set_xlim([0, 1])
+    plt.show()
 
 
 if __name__ == "__main__":
