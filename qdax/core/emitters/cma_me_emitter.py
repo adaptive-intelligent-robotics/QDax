a4b5ef5dfc2554df471c710a50bdc8cd167124de
Felix Chalumeau <f.chalumeau@instadeep.com>
2022-09-06 10:20:19 +0000

    WIP: cma me emitter

--- qdax/core/emitters/cma_me_emitter.py
+++ qdax/core/emitters/cma_me_emitter.py
@@ -14,53 +14,41 @@ from qdax.core.emitters.emitter import Emitter, EmitterState
 from qdax.types import Descriptor, ExtraScores, Fitness, Genotype, Gradient, RNGKey
 
 
-class CMAMEGAState(EmitterState):
+class CMAMEState(EmitterState):
     """
-    Emitter state for the CMA-MEGA emitter.
+    Emitter state for the CMA-ME emitter.
 
     Args:
-        theta: current genotype from where candidates will be drawn.
-        theta_grads: normalized fitness and descriptors gradients of theta.
         random_key: a random key to handle stochastic operations. Used for
             state update only, another key is used to emit. This might be
             subject to refactoring discussions in the future.
         cmaes_state: state of the underlying CMA-ES algorithm
     """
 
-    theta: Genotype
-    theta_grads: Gradient
     random_key: RNGKey
     cmaes_state: CMAESState
 
 
-class CMAMEGAEmitter(Emitter):
+class CMAMEEmitter(Emitter):
     def __init__(
         self,
-        scoring_function: Callable[
-            [Genotype, RNGKey], Tuple[Fitness, Descriptor, ExtraScores, RNGKey]
-        ],
         batch_size: int,
         learning_rate: float,
-        num_descriptors: int,
+        genotype_dim: int,
         sigma_g: float,
         step_size: Optional[float] = None,
     ):
         """
-        Class for the emitter of CMA Mega from "Differentiable Quality Diversity" by
-        Fontaine et al.
+        Class for the emitter of CMA ME from "Covariance Matrix Adaptation for the
+        Rapid Illumination of Behavior Space" by Fontaine et al.
 
         Args:
-            scoring_function: a function to score individuals, outputing fitness,
-                descriptors and extra scores. With this emitter, the extra score
-                contains gradients and normalized gradients.
             batch_size: number of solutions sampled at each iteration
             learning_rate: rate at which the mean of the distribution is updated.
-            num_descriptors: number of descriptors
+            genotype_dim: dimension of the genotype space.
             sigma_g: standard deviation for the coefficients
             step_size: size of the steps used in CMAES updates
         """
-        self._scoring_function = scoring_function
-        self._batch_size = batch_size
         self._learning_rate = learning_rate
         self._weights = jnp.expand_dims(
             jnp.log(batch_size + 0.5) - jnp.log(jnp.arange(1, batch_size + 1)), axis=-1
@@ -73,7 +61,7 @@ class CMAMEGAEmitter(Emitter):
         # define a CMAES instance
         self._cmaes = CMAES(
             population_size=batch_size,
-            search_dim=num_descriptors + 1,
+            search_dim=genotype_dim,
             # no need for fitness function in that specific case
             fitness_function=None,  # type: ignore
             num_best=batch_size,
@@ -87,7 +75,7 @@ class CMAMEGAEmitter(Emitter):
     @partial(jax.jit, static_argnames=("self",))
     def init(
         self, init_genotypes: Genotype, random_key: RNGKey
-    ) -> Tuple[CMAMEGAState, RNGKey]:
+    ) -> Tuple[CMAMEState, RNGKey]:
         """
         Initializes the CMA-MEGA emitter
 
@@ -100,22 +88,10 @@ class CMAMEGAEmitter(Emitter):
             The initial state of the emitter.
         """
 
-        # define init theta as 0
-        theta = jax.tree_map(
-            lambda x: jnp.zeros_like(x[:1, ...]),
-            init_genotypes,
-        )
-
-        # score it
-        _, _, extra_score, random_key = self._scoring_function(theta, random_key)
-        theta_grads = extra_score["normalized_grads"]
-
         # return the initial state
         random_key, subkey = jax.random.split(random_key)
         return (
-            CMAMEGAState(
-                theta=theta,
-                theta_grads=theta_grads,
+            CMAMEState(
                 cmaes_state=self._cma_initial_state,
                 random_key=subkey,
             ),
@@ -126,7 +102,7 @@ class CMAMEGAEmitter(Emitter):
     def emit(
         self,
         repertoire: Optional[MapElitesRepertoire],
-        emitter_state: CMAMEGAState,
+        emitter_state: CMAMEState,
         random_key: RNGKey,
     ) -> Tuple[Genotype, RNGKey]:
         """
@@ -142,27 +118,12 @@ class CMAMEGAEmitter(Emitter):
         Returns:
             New genotypes and a new random key.
         """
-
-        # retrieve elements from the emitter state
-        theta = jnp.nan_to_num(emitter_state.theta)
-        cmaes_state = emitter_state.cmaes_state
-
-        # get grads - remove nan and first dimension
-        grads = jnp.nan_to_num(emitter_state.theta_grads.squeeze(axis=0))
-
-        # Draw random coefficients - use the emitter state key
-        coeffs, _ = self._cmaes.sample(
-            cmaes_state=cmaes_state, random_key=emitter_state.random_key
+        # emit from CMA-ES
+        offsprings, _ = self._cmaes.sample(
+            cmaes_state=emitter_state.cmaes_state, random_key=emitter_state.random_key
         )
 
-        # make sure the fitness coefficient is positive
-        coeffs = coeffs.at[:, 0].set(jnp.abs(coeffs[:, 0]))
-        update_grad = coeffs @ grads.T
-
-        # Compute new candidates
-        new_thetas = jax.tree_map(lambda x, y: x + y, theta, update_grad)
-
-        return new_thetas, random_key
+        return offsprings, random_key
 
     @partial(
         jax.jit,
@@ -170,7 +131,7 @@ class CMAMEGAEmitter(Emitter):
     )
     def state_update(
         self,
-        emitter_state: CMAMEGAState,
+        emitter_state: CMAMEState,
         repertoire: MapElitesRepertoire,
         genotypes: Genotype,
         fitnesses: Fitness,
@@ -202,48 +163,31 @@ class CMAMEGAEmitter(Emitter):
 
         # retrieve elements from the emitter state
         cmaes_state = emitter_state.cmaes_state
-        theta = jnp.nan_to_num(emitter_state.theta)
-        grads = jnp.nan_to_num(emitter_state.theta_grads[0])
 
-        # Update the archive and compute the improvements
+        # Compute the improvements
         indices = get_cells_indices(descriptors, repertoire.centroids)
         improvements = fitnesses - repertoire.fitnesses[indices]
 
         sorted_indices = jnp.argsort(improvements)[::-1]
 
-        # Draw the coeffs - reuse the emitter state key to get same coeffs
-        coeffs, random_key = self._cmaes.sample(
-            cmaes_state=cmaes_state, random_key=emitter_state.random_key
-        )
-        # make sure the fitness coeff is positive
-        coeffs = coeffs.at[:, 0].set(jnp.abs(coeffs[:, 0]))
-
-        # get the gradients that must be applied
-        update_grad = coeffs @ grads.T
-
-        # weight terms - based on improvement rank
-        gradient_step = jnp.sum(self._weights[sorted_indices] * update_grad, axis=0)
-
-        # update theta
-        theta = jax.tree_map(
-            lambda x, y: x + self._learning_rate * y, theta, gradient_step
-        )
-
         # Update CMA Parameters
-        sorted_candidates = coeffs[sorted_indices]
+        # TODO: only works for array atm
+        sorted_candidates = genotypes[sorted_indices]
         cmaes_state = self._cmaes.update_state(cmaes_state, sorted_candidates)
 
         # If no improvement draw randomly and re-initialize parameters
         reinitialize = jnp.all(improvements < 0)
 
         # re-sample
-        random_theta, random_key = repertoire.sample(random_key, 1)
+        random_genotype, random_key = repertoire.sample(emitter_state.random_key, 1)
+
+        # TODO: this should be hidden in the CMAES as a method
+        # TODO: this is very ugly here
 
         # update - new state or init state if reinitialize is 1
-        theta = jnp.nan_to_num(theta) * (1 - reinitialize) + random_theta * reinitialize
-        mean = self._cma_initial_state.mean * reinitialize + jnp.nan_to_num(
-            cmaes_state.mean
-        ) * (1 - reinitialize)
+        mean = random_genotype * reinitialize + jnp.nan_to_num(cmaes_state.mean) * (
+            1 - reinitialize
+        )
         cov = self._cma_initial_state.cov_matrix * reinitialize + jnp.nan_to_num(
             cmaes_state.cov_matrix
         ) * (1 - reinitialize)
@@ -268,13 +212,8 @@ class CMAMEGAEmitter(Emitter):
             num_updates=num_updates,
         )
 
-        # score theta
-        _, _, extra_score, random_key = self._scoring_function(theta, random_key)
-
         # create new emitter state
-        emitter_state = CMAMEGAState(
-            theta=theta,
-            theta_grads=extra_score["normalized_grads"],
+        emitter_state = CMAMEState(
             cmaes_state=cmaes_state,
             random_key=random_key,
         )
