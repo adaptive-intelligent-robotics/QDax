c50817596f1e9c39de34afead56d5fc0d450595b
Thomas Pierrot <t.pierrot@instadeep.com>
2023-02-19 13:05:21 +0000

    working on fixing td3 loss fn mismatch between squid and qdax

--- qdax/baselines/td3_pbt.py
+++ qdax/baselines/td3_pbt.py
@@ -18,7 +18,6 @@ from qdax.core.neuroevolution.buffers.buffer import (
     ReplayBuffer,
     Transition,
 )
-from qdax.core.neuroevolution.losses.td3_loss import make_td3_loss_fn
 from qdax.core.neuroevolution.mdp_utils import get_first_episode
 from qdax.core.neuroevolution.networks.td3_networks import make_td3_networks
 from qdax.core.neuroevolution.sac_utils import do_iteration_fn, generate_unroll
@@ -128,6 +127,85 @@ class PBTTD3Config:
     soft_tau_update: float = 0.005
     critic_hidden_layer_size: Tuple[int, ...] = (256, 256)
     policy_hidden_layer_size: Tuple[int, ...] = (256, 256)
+
+
+def make_td3_loss_fn(
+    policy_fn: Callable[[Params, Observation], jnp.ndarray],
+    critic_fn: Callable[[Params, Observation, Action], jnp.ndarray],
+) -> Tuple[
+    Callable[[Params, Params, Transition], jnp.ndarray],
+    Callable[[Params, Params, Params, Transition, RNGKey], jnp.ndarray],
+]:
+    """Creates the loss functions for TD3.
+
+    Args:
+        policy_fn: forward pass through the neural network defining the policy.
+        critic_fn: forward pass through the neural network defining the critic.
+
+    Returns:
+        Return the loss functions used to train the policy and the critic in TD3.
+    """
+
+    @jax.jit
+    def _policy_loss_fn(
+        policy_params: Params,
+        critic_params: Params,
+        transitions: Transition,
+    ) -> jnp.ndarray:
+        """Policy loss function for TD3 agent"""
+
+        action = policy_fn(policy_params, transitions.obs)
+        q_value = critic_fn(
+            critic_params, obs=transitions.obs, actions=action  # type: ignore
+        )
+        q1_action = jnp.take(q_value, jnp.asarray([0]), axis=-1)
+        policy_loss = -jnp.mean(q1_action)
+        return policy_loss
+
+    @jax.jit
+    def _critic_loss_fn(
+        critic_params: Params,
+        target_policy_params: Params,
+        target_critic_params: Params,
+        transitions: Transition,
+        random_key: RNGKey,
+        reward_scaling: float,
+        discount: float,
+        noise_clip: float,
+        policy_noise: float,
+    ) -> jnp.ndarray:
+        """Critics loss function for TD3 agent"""
+        noise = (
+            jax.random.normal(random_key, shape=transitions.actions.shape)
+            * policy_noise
+        ).clip(-noise_clip, noise_clip)
+
+        next_action = (
+            policy_fn(target_policy_params, transitions.next_obs) + noise
+        ).clip(-1.0, 1.0)
+        next_q = critic_fn(  # type: ignore
+            target_critic_params, obs=transitions.next_obs, actions=next_action
+        )
+        next_v = jnp.min(next_q, axis=-1)
+        target_q = jax.lax.stop_gradient(
+            transitions.rewards * reward_scaling
+            + (1.0 - transitions.dones) * discount * next_v
+        )
+        q_old_action = critic_fn(  # type: ignore
+            critic_params,
+            obs=transitions.obs,
+            actions=transitions.actions,
+        )
+        q_error = q_old_action - jnp.expand_dims(target_q, -1)
+
+        # Better bootstrapping for truncated episodes.
+        q_error = q_error * jnp.expand_dims(1.0 - transitions.truncations, -1)
+        q_losses = jnp.mean(jnp.square(q_error), axis=-2)
+        q_loss = jnp.sum(q_losses, axis=-1)
+
+        return q_loss
+
+    return _policy_loss_fn, _critic_loss_fn
 
 
 class PBTTD3:
@@ -425,7 +503,9 @@ class PBTTD3:
         true_returns = jnp.nansum(transitions.rewards, axis=0)
         true_return = jnp.mean(true_returns, axis=-1)
 
-        transitions = jax.tree_util.tree_map(lambda x: jnp.swapaxes(x, 0, 1), transitions)
+        transitions = jax.tree_util.tree_map(
+            lambda x: jnp.swapaxes(x, 0, 1), transitions
+        )
         masks = jnp.isnan(transitions.rewards)
         bds = bd_extraction_fn(transitions, masks)
 
