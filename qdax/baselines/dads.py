785e9aba9f28b7f27422ea132083f0fb5363e96b
Thomas Pierrot <t.pierrot@instadeep.com>
2023-02-27 15:04:37 -0500

    fixed dads test

--- qdax/baselines/dads.py
+++ qdax/baselines/dads.py
@@ -88,8 +88,11 @@ class DADS(SAC):
         )
 
         # define the action distribution
-        parametric_action_distribution = NormalTanhDistribution(event_size=action_size)
-        self._sample_action_fn = parametric_action_distribution.sample
+        self._action_size = action_size
+        self._parametric_action_distribution = NormalTanhDistribution(
+            event_size=action_size
+        )
+        self._sample_action_fn = self._parametric_action_distribution.sample
 
         # define the losses
         (
@@ -105,7 +108,7 @@ class DADS(SAC):
             discount=self._config.discount,
             action_size=action_size,
             num_skills=self._config.num_skills,
-            parametric_action_distribution=parametric_action_distribution,
+            parametric_action_distribution=self._parametric_action_distribution,
         )
 
         # define the optimizers
@@ -434,7 +437,7 @@ class DADS(SAC):
 
         dynamics_loss, dynamics_gradient = jax.value_and_grad(self._dynamics_loss_fn,)(
             training_state.dynamics_params,
-            transitions,
+            transitions=transitions,
         )
 
         (dynamics_updates, dynamics_optimizer_state,) = self._dynamics_optimizer.update(
@@ -499,13 +502,11 @@ class DADS(SAC):
             alpha_loss,
             random_key,
         ) = self._update_alpha(
+            alpha_lr=self._config.learning_rate,
             training_state=training_state,
             transitions=transitions,
             random_key=random_key,
         )
-
-        # use the previous alpha
-        alpha = jnp.exp(training_state.alpha_params)
 
         # update critic
         (
@@ -515,8 +516,10 @@ class DADS(SAC):
             critic_loss,
             random_key,
         ) = self._update_critic(
+            critic_lr=self._config.learning_rate,
+            reward_scaling=self._config.reward_scaling,
+            discount=self._config.discount,
             training_state=training_state,
-            alpha=alpha,
             transitions=transitions,
             random_key=random_key,
         )
@@ -528,8 +531,8 @@ class DADS(SAC):
             policy_loss,
             random_key,
         ) = self._update_actor(
+            policy_lr=self._config.learning_rate,
             training_state=training_state,
-            alpha=alpha,
             transitions=transitions,
             random_key=random_key,
         )
@@ -554,7 +557,7 @@ class DADS(SAC):
             "critic_loss": critic_loss,
             "dynamics_loss": dynamics_loss,
             "alpha_loss": alpha_loss,
-            "alpha": alpha,
+            "alpha": jnp.exp(alpha_params),
             "training_observed_reward_mean": jnp.mean(transitions.rewards),
             "target_mean": jnp.mean(transitions.next_state_desc),
             "target_std": jnp.std(transitions.next_state_desc),
