1b131ff48a87b2189c160eddb7cad557606f2cfe
lc1021 <lc1021@ic.ac.uk>
2024-02-12 17:07:20 +0000

    Fix using jax.Array

--- qdax/types.py
+++ qdax/types.py
@@ -45,5 +45,5 @@ class ParetoFront(Generic[T]):
 Mask: TypeAlias = jnp.ndarray
 
 # Others
-RNGKey: TypeAlias = jax.random.Array
+RNGKey: TypeAlias = jax.Array
 Metrics: TypeAlias = Dict[str, jnp.ndarray]
