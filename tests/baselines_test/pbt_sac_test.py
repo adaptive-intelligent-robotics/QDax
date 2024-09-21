6656f5e31849f4adc19b6331a2cc00c9000e6f61
Luca Grillotti <luca.grillotti16@imperial.ac.uk>
2024-09-21 12:57:25 +0100

    fix: Change to new-style-jax-rng-keys. (#195)
    * fix: Change to new-style-jax-rng-keys.
    
    ---------
    
    Co-authored-by: Milton Montero <lleramontero@gmail.com>

--- tests/baselines_test/pbt_sac_test.py
+++ tests/baselines_test/pbt_sac_test.py
@@ -76,7 +76,7 @@ def test_pbt_sac() -> None:
 
         return env_states, eval_env_first_states
 
-    key = jax.random.PRNGKey(seed)
+    key = jax.random.key(seed)
     key, *keys = jax.random.split(key, num=1 + num_devices)
     keys = jnp.stack(keys)
     env_states, eval_env_first_states = jax.pmap(
@@ -104,6 +104,9 @@ def test_pbt_sac() -> None:
         observation_size=env.observation_size,
         buffer_size=buffer_size,
     )
+
+    # Need to convert to PRNGKey because of github.com/jax-ml/jax/issues/23647
+    keys = jax.random.key_data(keys)
     keys, training_states, replay_buffers = jax.pmap(
         agent_init_fn, axis_name="p", devices=devices
     )(keys)
